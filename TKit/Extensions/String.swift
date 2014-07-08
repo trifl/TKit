0df2cc54a92862d5b6c468e8a4ed47aea3d6a6eb
JP McGlone <jp@trifl.co>
2014-07-07 17:31:53 -0700

    added extensions for String and Float

---
+++ TKit/Extensions/String.swift
@@ -0,0 +1,15 @@
+//
+//  String.swift
+//  TKit
+//
+//  Created by JP McGlone on 7/7/14.
+//  Copyright (c) 2014 trifl. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var length: Int { return countElements(self) }
+    
+}
