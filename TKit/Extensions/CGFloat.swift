cf9d02866003e2c4bce65c8c47e6f160a899ea4f
JP McGlone <jp@trifl.co>
2014-07-22 21:02:54 -0700

    beta 4 changes

---
+++ TKit/Extensions/CGFloat.swift
@@ -0,0 +1,25 @@
+//
+//  CGFloat.swift
+//  TKit
+//
+//  Created by JP McGlone on 7/7/14.
+//  Copyright (c) 2014 trifl. All rights reserved.
+//
+
+import Foundation
+
+extension CGFloat {
+    // common 
+    var thrice: CGFloat { return self * 3.0 }
+    var twice: CGFloat { return self * 2.0 }
+    var half: CGFloat { return self * 0.5 }
+    var third: CGFloat { return self / 3.0 }
+    var fourth: CGFloat { return self * 0.25 }
+    var sixth: CGFloat { return self / 6.0 }
+    var eighth: CGFloat { return self * 0.125 }
+    var twelfth: CGFloat { return self / 12.0 }
+    var sixteenth: CGFloat { return self * 0.0625 }
+    
+    //
+    var inverse: CGFloat { return 1.0 / self }
+}
---
+++ TKit/TKModel/TKModel.swift
@@ -0,0 +1,21 @@
+//
+//  TKModel.swift
+//  TKit
+//
+//  Created by JP McGlone on 7/13/14.
+//  Copyright (c) 2014 trifl. All rights reserved.
+//
+
+import Foundation
+
+class TKModel : NSObject {
+    // For integer IDs
+    var id = 0
+    
+    // For all other IDs
+    var sid = String()
+    
+    func initWithJSON() {
+        
+    }
+}
