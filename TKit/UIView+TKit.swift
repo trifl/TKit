c23bf87918cf2ac3a4670fa62495bf676ef1b45c
JP McGlone <jp@trifl.co>
2015-06-09 18:17:52 -0700

    version 1 of tk_layout(orientation, views) works :D

--- TKit/UIView+TKit.swift
+++ TKit/UIView+TKit.swift
@@ -9,6 +9,10 @@
 import UIKit
 import Foundation
 
+public enum TKLayoutOrientation {
+  case Horizontal, Vertical
+}
+
 public extension UIView {
   // TODO: determine if we need to setNeedsLayout or not. If not, do we really even need these extensions?
   // ...decisions.
@@ -73,5 +77,55 @@ public extension UIView {
   public func tk_moveToView(view: UIView?) {
     view?.addSubview(self)
     self.frame = tk_frameInView(view)
+  }
+  
+  // MARK: Layout helpers
+  /**
+  layout your views horizontally or vertically
+  :param: orienation the orientation you want to lay out in, .Horizontal or .Vertical
+  :views: the array of views and/or floats that represent relative spacing 
+  
+  Example:
+  
+  - [1, view1, 0.5, view2, 1] will space out view1 and view2 such that the margin of the left and the right are the same, but the space between the views will be half that of the margin
+  */
+  public func tk_layout(orientation: TKLayoutOrientation, views: [AnyObject]) {
+    // TODO: vertical
+    var viewTotalD = 0 as CGFloat
+    var totalSpacing = 0 as CGFloat
+    for obj in views {
+      if let view = obj as? UIView {
+        if orientation == .Horizontal {
+          viewTotalD += view.tk_width
+        } else if orientation == .Vertical {
+          viewTotalD += view.tk_height
+        }
+      } else {
+        totalSpacing += obj as! CGFloat
+      }
+    }
+    
+    var leftoverD: CGFloat!
+    if orientation == .Horizontal {
+      leftoverD = self.tk_width - viewTotalD
+    } else if orientation == .Vertical {
+      leftoverD = self.tk_height - viewTotalD
+    }
+    var ratio = leftoverD / totalSpacing as CGFloat
+    
+    var d = 0 as CGFloat
+    for obj in views {
+      if let view = obj as? UIView {
+        if orientation == .Horizontal {
+          view.tk_left = d
+          d = view.tk_right
+        } else if orientation == .Vertical {
+          view.tk_top = d
+          d = view.tk_bottom
+        }
+      } else {
+        d += ratio * (obj as! CGFloat)
+      }
+    }
   }
 }
--- TKitDemo/ViewController.swift
+++ TKitDemo/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import TKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    var view1 = UIView(frame: CGRectMake(0, 100, 25, 25))
+    view1.backgroundColor = .greenColor()
+    view.addSubview(view1)
+    
+    var view2 = UIView(frame: CGRectMake(0, 100, 25, 25))
+    view2.backgroundColor = .redColor()
+    view.addSubview(view2)
+    
+    var view3 = UIView(frame: CGRectMake(0, 100, 25, 25))
+    view3.backgroundColor = .redColor()
+    view.addSubview(view3)
+    
+    view.tk_layout(.Horizontal, views: [1, view1, 0.5, view2, 0.5, view3,  1])
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
 
 }
 
