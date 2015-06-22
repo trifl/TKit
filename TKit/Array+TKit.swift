d2425bf4dd9b4b5ef51899ca3627eda1d7c901a6
JP McGlone <jp@trifl.co>
2015-06-21 18:26:51 -0700

    added Array extension for removing an object from the array

---
+++ TKit/Array+TKit.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+extension Array {
+  mutating func tk_remove<U: Equatable>(object: U) -> Bool {
+    for (idx, objectToCompare) in enumerate(self) {
+      if let to = objectToCompare as? U {
+        if object == to {
+          self.removeAtIndex(idx)
+          return true
+        }
+      }
+    }
+    return false
+  }
+}
