54c3519c85282489ef1dd563707b6105be29e1dc
JP McGlone <jp@trifl.co>
2015-09-12 22:15:54 -0700

    cleaned up UIColor extension

--- TKit/UIColor+TKit.swift
+++ TKit/UIColor+TKit.swift
@@ -1,14 +1,10 @@
 import UIKit
 
 extension UIColor {
-  public class func tk_color(hex:Int, alpha: CGFloat) -> UIColor {
+  public class func tk_color(hex:Int, alpha: CGFloat = 0.0) -> UIColor {
     let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
     let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
     let blue = CGFloat((hex & 0xFF)) / 255.0
     return UIColor(red: red, green: green, blue: blue, alpha: alpha)
-  }
-  
-  public class func tk_color(hex:Int) -> UIColor {
-    return UIColor.tk_color(hex, alpha: 1)
   }
 }
